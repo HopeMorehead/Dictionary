7dc453c83327548bbdbe29409f221698acd3dfd0
HopeMorehead <splendidwonder@gmail.com>
2017-07-12 14:18:52 -0400

    adding more keyvaluepairs

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -10,6 +10,52 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
+
+
+            //syntax for dictionary
+
+          /*  Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            {
+
+                //A1 key twix value
+                {"A1", "Twix" },
+                {"A2","Twizzlers" },
+                {"A3", "Reeses" },
+                {"A4", "Sour Patch kids"}
+
+
+            };
+              foreach (KeyValuePair<string, string> item in snackMachine)
+            //  foreach (var item in snackMachine)
+
+            {
+
+                Console.WriteLine(item);
+
+            }
+
+            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            drinkMachine.Add(10, "Ginger Ale");
+            drinkMachine.Add(11, "Bottled water");
+            drinkMachine.Add(12, "Orange Juice");
+            drinkMachine.Add(13, "Fruit Punch");
+            drinkMachine.Add(14, "Mountain Dew Code Red");
+            //counting the number of entries in the dictionary
+            Console.WriteLine(drinkMachine.Count);
+
+            foreach (KeyValuePair<int,string> drink in drinkMachine)
+            {
+                Console.WriteLine(drink.Key);
+                Console.WriteLine(drink.Value);
+
+
+
+            }*/
+
+
+
+
+
         }
     }
 }
